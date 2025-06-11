57fa31c009b1edc34211bb567c9f87581bbfb179
André Ruffert <github@andreruffert.com>
2025-06-11 14:10:52 +0200

    feat: require Node.js 18 and move to ESM
    BREAKING CHANGE: require Node.js >=18 and move to ESM

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
  * @param  {Number} num
  * @return {Number}
  */
-function closestNumber(arr, num) {
+export default function closestNumber(arr, num) {
   return arr.reduce((prev, curr) => (Math.abs(curr - num) < Math.abs(prev - num)) ? curr : prev);
 }
-
-module.exports = closestNumber;
